d568f1fe8149d3a7b9ad2f470378ddabdcb59478
Tokmak Abdullah <tokmaka1@l23-0164.org.aalto.fi>
2025-01-21 17:52:13 +0200

    Latent variable approach

--- mas-toy.py
+++ mas-toy.py
@@ -3,6 +3,8 @@ import torch
 import gpytorch
 import matplotlib.pyplot as plt
 import warnings
+import copy
+from tqdm import tqdm
 
 
 # General function definitions
@@ -24,7 +26,8 @@ class ground_truth():
         self.fX = torch.tensor(self.f(self.X_plot), dtype=torch.float32)
         self.safety_threshold = np.quantile(self.fX, 0.3)  # np.quantile(self.fX, np.random.uniform(low=0.15, high=0.5))
 
-        # f(x) = (x1 + x2) - (y_1 + y_2); Then this is just 4-dimensional right? Yes? Yes.
+    def conduct_experiment(self, x, noise_std):
+        return torch.tensor(self.f(x))  # noise-free in the beginning + #np.random.normal(loc=0, scale=noise_std, size=1), dtype=x.dtype)
 
 
 class GPRegressionModel(gpytorch.models.ExactGP):  # this model has to be build "new"
@@ -68,7 +71,6 @@ def initial_safe_samples(gt, num_safe_points):
     return X_sample, Y_sample
 
 
-
 # Create function
 lengthscale = 0.1
 noise_std = 1e-2
@@ -84,11 +86,9 @@ Y_sample = Y_sample_init.clone()
 
 
 class agent_class():
-    def __init__(self, delta_confidence, noise_std, number, X_plot, own_actions, action_vector,
-                Y_sample, exploration_threshold, gt):
-        self.CCP
+    def __init__(self, delta_confidence, noise_std, number, X_plot, actions, exploration_threshold, gt):
         self.gt = gt  # at least for toy experiments it works like this.
-        # self.RKHS_norm 
+        # self.RKHS_norm
         self.exploration_threshold = exploration_threshold
         self.delta_confidence = delta_confidence
         self.X_plot = X_plot
@@ -97,10 +97,9 @@ class agent_class():
         self.safety_threshold = gt.safety_threshold
         self.number = number
         self.lambda_bar = max(self.noise_std, 1)
-        self.own_actions = own_actions
-        self.action_vector = action_vector
-        self.global_reward = Y_sample
-        self.discr_domain = X_plot
+        self.actions = actions
+        self.rewards = []  # just initialize
+        self.action_domain = X_plot
 
     def compute_actions_reward_model(self, gpr):
         self.actions_reward_model = gpr(train_x=self.action_vector, train_y=self.global_reward, noise_std=self.noise_std, lengthscale=0.1)
@@ -108,62 +107,30 @@ class agent_class():
     def compute_predicting_others_model(self, gpr):
         self.predicting_others_model = gpr(train_x=self.own_actions, train_y=self.action_vector, noise_std=self.noise_std, lengthscale=0.1)
 
-    def compute_mean_var(self):
-        self.actions_reward_model.eval()
-        self.f_preds = self.actions_reward_model(self.discr_domain)
-        self.mean = self.f_preds.mean
-        self.var = self.f_preds.variance
+    def compute_mean_var_confidence_intervals_GP1(self, domain):
+        self.GP1.eval()
+        self.GP1.f_preds = self.GP1(domain)
+        self.GP1.mean = self.GP1.f_preds.mean
+        self.GP1.var = self.GP1.f_preds.variance
+        # warnings.warn("Somewhere RKHS norm")
+        self.GP1.beta = self.compute_beta()
+        self.GP1.lcb = self.GP1.mean - self.GP1.beta*torch.sqrt(self.GP1.var)
+        self.GP1.ucb = self.GP1.mean + self.GP1.beta*torch.sqrt(self.GP1.var)
 
-    def compute_confidence_intervals(self):
-        warnings.warn("Somewhere RKHS norm")
-        self.compute_beta()
-        self.lcb = self.mean - self.beta*torch.sqrt(self.var)  # we have to use standard deviation instead of variance
-        self.ucb = self.mean + self.beta*torch.sqrt(self.var)
+    def compute_mean_var_confidence_intervals_GP2(self):
+        self.GP2.eval()
+        self.GP2.f_preds = self.GP2(self.action_domain)
+        self.GP2.mean = self.GP2.f_preds.mean
+        self.GP2.var = self.GP2.f_preds.variance
+        # warnings.warn("Somewhere RKHS norm")
+        self.GP2.beta = self.compute_beta()
+        self.GP2.lcb = self.GP2.mean - self.GP2.beta*torch.sqrt(self.GP2.var)
+        self.GP2.ucb = self.GP2.mean + self.GP2.beta*torch.sqrt(self.GP2.var)
 
-    def compute_safe_set(self):
-        self.S = self.lcb > self.safety_threshold
-        warnings.warn("Computing safe set in the <pessimistic and lazy> way")
-
-        # Auxiliary objects of potential maximizers M and potential expanders G
-        self.G = self.S.clone()
-        self.M = self.S.clone()
-
-    def maximizer_routine(self, best_lower_bound_others):
-        self.M[:] = False  # initialize
-        self.max_M_var = 0  # initialize
-        if not torch.any(self.S):  # no safe points
-            return
-        self.best_lower_bound_local = max(self.lcb[self.S])
-        self.M[self.S] = self.ucb[self.S] >= max(best_lower_bound_others, self.best_lower_bound_local)
-        self.M[self.M.clone()] = (self.ucb[self.M] - self.lcb[self.M]) > self.exploration_threshold
-        if not torch.any(self.M):
-            return
-        self.max_M_var = torch.max(self.ucb[self.M] - self.lcb[self.M])
-        self.max_M_ucb = torch.max(self.ucb[self.M])
-
-    def expander_routine(self):
-        self.G[:] = False  # initialize
-        if not torch.any(self.S) or torch.all(self.S):  # no safe points or all of them are safe points
-            return
-        # no need to consider points in M
-        if self.compute_all_sets:  # for visualization; introductory example
-            s = self.S.clone()
-        else:
-            s = torch.logical_and(self.S, ~self.M)
-            s[s.clone()] = (self.ucb[s] - self.lcb[s]) > self.max_M_var
-            s[s.clone()] = (self.ucb[s] - self.lcb[s]) > self.exploration_threshold
-        if not torch.any(s):
-            return
-        potential_expanders = self.discr_domain[s]
-        unsafe_points = self.discr_domain[~self.S]
-        kernel_distance = self.compute_kernel_distance(potential_expanders, unsafe_points)
-        ucb_expanded = self.ucb[s].unsqueeze(1).expand(-1, kernel_distance.size(1))
-        s[s.clone()] = torch.any(ucb_expanded - self.B*kernel_distance > self.safety_threshold, dim=1)
-        self.G = s
-        warnings.warn("Computing expanders in discrete way like Sui et al. 2015")
 
     def compute_beta(self):
         # Fiedler et al. 2024 Equation (7); based on Abbasi-Yadkori 2013
+        return 3
         inside_log = torch.det(torch.eye(len(self.global_rewards)) + (1/self.noise_std*self.K))
         inside_sqrt = self.noise_std*torch.log(inside_log) - (2*self.noise_std*torch.log(torch.tensor(self.delta_confidence)))
         self.beta = self.B + torch.sqrt(inside_sqrt)
@@ -186,23 +153,12 @@ class agent_class():
         # To update models(s), samples etc. No need to build the agent completely new
         # Not important right now. We will switch to BOTorch anyways so...
 
-    def predict_action_space(self):
-        self.compute_predicting_others_model(gpr=GPRegressionModel)
-        self.compute_predicting_others_model.eval()
-        self.f_preds_predicting_others_model = self.predicting_others_model(self.own_actions[-1])  # we give the own action(s), and get the others one action!
-        last_action_space = self.f_preds_predicting_others_model.mean
-        # Do some concatenation stuff here
-        self.action_space = last_action_space  # combined with ...
-        # return last_action_space
 
-    def process_communication(self):
-        return action_space
+    def GPUCB(self, GP):
+        # acquisition function
+        max_index = torch.argmax(GP.ucb)
+        return self.action_domain[max_index]  # action VECTOR!
 
-    def opt(self):
-        if not torch.any(torch.logical_or(self.M, self.G)):
-            raise Exception("No interesting point to evaluate!")
-        x_next = self.discr_domain[torch.logical_or(self.M, self.G)][torch.argmax(self.var[torch.logical_or(self.M, self.G)])]
-        return x_next
 
 
 # Hyperparameters
@@ -211,43 +167,67 @@ delta_confidence = 0.1  # yields 90% confidence for safety proof.
 num_safe_points = 1  # singleton safe set
 num_iterations = 10  # number of total points at the end
 exploration_threshold = 0.1  # let us start with that, maybe we should decrease to 0. But let's see
-n_dimensions = 2  # 2 agents with 2 dimensions; but then we cannot really plot
+n_dimensions = 2  # 2 agents with each 1 dimension
 points_per_axis = 500  # 30 for 4D, 1000 for 1D, 500 for 2D, 100 for 3D, 15 for 5D
 X_plot = compute_grid(n_dimensions, points_per_axis)
 # We also need one-dimensional X_plot for the other GP
 RKHS_norm = 1  # RKHS norm of that one ground truth
 gt = ground_truth(num_center_points=1000, X_plot=X_plot, RKHS_norm=RKHS_norm)
-action_vector_init, global_reward_init = initial_safe_samples(gt=gt, num_safe_points=num_safe_points)  # this is wrong btw the ground truth needs 2 2-dimensional variables
+action_vector_init, _ = initial_safe_samples(gt=gt, num_safe_points=num_safe_points)  # this is wrong btw the ground truth needs 2 2-dimensional variables
 action_vector = action_vector_init.clone()
-global_reward = global_reward_init.clone()
+# global_reward = global_reward_init.clone()
 
-agent0 = agent_class(delta_confidence, noise_std, 0, X_plot, action_vector[:, 0], action_vector, global_reward, exploration_threshold, gt)
-agent1 = agent_class(delta_confidence, noise_std, 1, X_plot, action_vector[:, 1], action_vector, global_reward, exploration_threshold, gt)
+agent0 = agent_class(delta_confidence, noise_std, 0, X_plot, action_vector[:, 0], exploration_threshold, gt)
+agent1 = agent_class(delta_confidence, noise_std, 1, X_plot, action_vector[:, 1], exploration_threshold, gt)
 list_agents = [agent0, agent1]
 
 # self, delta_confidence, noise_std, number, X_plot, own_actions, action_vector
 
-# But we need something to begin with... So we have the complete action space in the beginning
+# But we need something to begin with... So we have the complete action space in the beginning; no
 
-for i in range(num_iterations):  # 10 steps
+# Initialize
+rewards = torch.tensor(gt.conduct_experiment(x=torch.tensor([agent0.actions, agent1.actions]), noise_std=0), dtype=torch.float32)  # also; initialize the list
+GP1 = GPRegressionModel(train_x=torch.tensor([agent0.actions, agent1.actions]).unsqueeze(0), train_y=rewards, noise_std=noise_std)
+agent0.GP1 = copy.deepcopy(GP1)
+agent1.GP1 = copy.deepcopy(GP1)
+agent0.GP2 = GPRegressionModel(torch.tensor([agent0.actions, rewards]).unsqueeze(0), agent1.actions, noise_std)
+agent1.GP2 = GPRegressionModel(torch.tensor([agent1.actions, rewards]).unsqueeze(0), agent0.actions, noise_std)
+
+
+for i in tqdm(range(1, num_iterations)):  # We do not start with 0; initialization is before. Does not make a big difference
     for agent in list_agents:
-        if i != 0 or (bool(i % 2) and agent.number == 0) or \
-           (not bool(i % 2) and agent.number == 1):
-            agent.process_communication(communicated_action)  # updates self.action_vector
-        elif i != 0:
-            agent.predict_action_space()  # updates self.action_vector
-        '''
-        The action vector at this very moment is complete in the sense of it has all the action
-        (communicated or predicted)
-        '''
-        agent.compute_actions_reward_model(gpr=GPRegressionModel)
-        agent.compute_mean_var()
-        agent.maximizer_routine()
-        agent.expander_routine()
-
-        # This is the information that will be processed by the next guy in the next iteration
-        communicated_action = agent.own_actions[-1]
-
+        # agent.compute_mean_var_confidence_intervals_GP1(domain=1)  # domain is questionable here; also not that important...
+        agent.compute_mean_var_confidence_intervals_GP2()
+        action_vector = agent.GPUCB(agent.GP2)
+        agent.actions = torch.cat((agent.actions, action_vector[agent.number].unsqueeze(0)), dim=0)
+        # We need both [in programming] because we need the reward
+        # Predict whole action space GP2
+        # Apply my action and receive reward
+    # Conduct experiment
+    reward = torch.tensor(gt.conduct_experiment(x=torch.tensor([agent0.actions[-1], agent1.actions[-1]]), noise_std=0), dtype=torch.float32)
+    rewards = torch.cat((rewards, reward), dim=0)
+    list_agents = list_agents[::1] if i%2 == list_agents[0].number else list_agents[::-1]  # permutate the list of agents such that the first entry in the list always communicates.
+    for agent in list_agents:
+        if i % 2 == agent.number:  # This is always the case
+            communicated_action = agent.actions[-1]  # communicate last action
+            # Determine action vector with now additional info of reward
+            # First, determine the GP: Build it with training data and predict using this one new point!; already built usin this one training data point I feel
+            agent.compute_mean_var_confidence_intervals_GP1(domain=torch.tensor([agent.actions[-1], reward]).unsqueeze(0))
+            others_action = agent.GP1.mean  # just the mean prediction is our prediction of what the other person is doing
+            others_actions = torch.cat((agent.GP1.train_inputs[0][0][1].view(1), others_action), dim=0) if len(agent.GP1.train_inputs[0][0].shape) == 1 else \
+                                        torch.cat((agent.GP1.train_inputs[0][0][:, 1].view(-1,1), others_action), dim=0)
+            # Update GP2 here and then remove last action because we do not want to use that for GP1??
+        else:
+            others_action = communicated_action.unsqueeze(0)
+            # Update GP1
+            # We now re-define the GP, which is fine. Adding point might be computationally cheaper and better, but not that important right now.
+            others_actions = torch.cat((agent.GP1.train_inputs[0][0][1].view(1), others_action), dim=0) if len(agent.GP1.train_inputs[0][0].shape) == 1 else \
+                                        torch.cat((agent.GP1.train_inputs[0][0][:, 1].view(-1, 1), others_action), dim=0) 
+            train_x = torch.cat((agent.actions.view(-1, 1), others_actions.view(-1, 1)), dim=1)  # one row has the actions from iteration 1 from both agents
+            agent.GP1 = GPRegressionModel(train_x=train_x.unsqueeze(0), train_y=rewards, noise_std=noise_std)
+        # Update GP2
+        agent.GP2 = GPRegressionModel(torch.stack([agent.actions, rewards], dim=1), others_actions, noise_std)  # correct dimension for the train_x?
+print('Hello')
 '''
 The problem is also we need to optimize within each agent, then send the current idea to the global reward system and then receive an input
 We need another GP to learn about the other's action. Or some other function that basically predicts what the other agent is doing. This can for 
